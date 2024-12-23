1a2a859d09805f4314fbc3bf0ecfe59a736b351d
RagulKannan2005 <msdragul154@gmail.com>
2024-12-23 22:57:26 +0530

    Update Registerform.jsx

--- src/components/Registerform.jsx
+++ src/components/Registerform.jsx
@@ -8,17 +8,17 @@ const Registerform = () => {
         <form action="#">
           <div className="input-container">
             <label htmlFor="fname">Full Name</label>
-            <input type="text" id="fname" />
+            <input type="text" id="fname" placeholder="Enter your Fullname"/>
             <label htmlFor="lname">Last Name</label>
-            <input type="text" id="lname" />
+            <input type="text" id="lname" placeholder="Enter your last name"/>
             <label htmlFor="Password">Password</label>
-            <input type="password" id="Password" />
+            <input type="password" id="Password" placeholder="Enter your password"/>
             <label htmlFor="Re-Password">Re-Password</label>
-            <input type="password" id="Re-Password" />
+            <input type="password" id="Re-Password" placeholder="Enter your password again" />
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
+            <input type="email" id="email" placeholder="Enter your email id" />
             <label htmlFor="phone">Phone</label>
-            <input type="number" id="phone" />
+            <input type="number" id="phone" placeholder="Enter your phone number"/>
             <label htmlFor="blood-group">Select Your Blood Group</label>
             <select id="blood-group" name="blood-group" defaultValue="" required>
               <option value="" disabled>
@@ -38,6 +38,8 @@ const Registerform = () => {
             <input type="radio" name="Gender" id="" />Female
             <label htmlFor="Dateofbirth">Date Of Birth</label>
             <input type="date" id="Dateofbirth" />
+            <label htmlFor="qrcode">Attach your QR</label>
+            
           </div>
         </form>
       </div>
