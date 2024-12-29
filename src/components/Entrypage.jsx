a152dea0785e2658934a79719fd097b8fa32123c
RagulKannan2005 <msdragul154@gmail.com>
2024-12-29 20:57:15 +0530

    Update Entrypage.jsx

--- src/components/Entrypage.jsx
+++ src/components/Entrypage.jsx
@@ -4,12 +4,16 @@ import "./Entrypage.css";
 const Entrypage = () => {
   return (
     <div className="page-container">
-      <h1>Welcome to Blood Doner Website</h1>
-      <div className="slogen"><span>Share Life, Give Hope.</span></div>
-      <div className="button-container">
-        <button className="btn btn-seeker">Seeker</button>
-        <button className="btn btn-doner">Doner</button>
-        <button className="btn btn-testcenter">Test center</button>
+      <div className="main-container">
+        <h1>Welcome to Blood Doner Website</h1>
+        <div className="slogen">
+          <span>Share Life, Give Hope.</span>
+        </div>
+        <div className="button-container">
+          <button className="btn ">Seeker</button>
+          <button className="btn ">Doner</button>
+          <button className="btn ">Test center</button>
+        </div>
       </div>
     </div>
   );
