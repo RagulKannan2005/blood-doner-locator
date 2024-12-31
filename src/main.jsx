efd94a82972555a4e32aace81c9bb77f1bef490b
RagulKannan2005 <msdragul154@gmail.com>
2024-12-31 14:43:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Seeker from './components/Seeker'
+// import Seeker from './components/Seeker'
 // import Registerform from './components/Registerform'
 // import Entrypage from './components/Entrypage'
 // import Tcenter from './components/Tcenter'
-// import App from './App.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <Registerform /> */}
     {/* <Entrypage /> */}
     {/* <Tcenter /> */}
     {/* <Seeker /> */}
+    
   </StrictMode>,
 )
