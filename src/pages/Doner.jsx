54d57e26d5cfcc19a1e86689093c548faa74a8eb
RagulKannan2005 <msdragul154@gmail.com>
2024-12-31 15:39:16 +0530

    Create Doner.jsx

---
+++ src/pages/Doner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+
+const Doner = () => {
+  return (
+    <>
+        <Navbar/>
+        <h2>Doner page</h2>
+    </>
+  )
+}
+
+export default Doner
