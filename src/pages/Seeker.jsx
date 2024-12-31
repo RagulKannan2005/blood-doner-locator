6894d59b5b627502308361b69bf8e87998cfe643
RagulKannan2005 <msdragul154@gmail.com>
2024-12-31 15:39:23 +0530

    Create Seeker.jsx

---
+++ src/pages/Seeker.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+
+const Seeker = () => {
+  return (
+    <>
+        <Navbar/>
+        <h2>Seeker page</h2>
+    </>
+  )
+}
+
+export default Seeker
