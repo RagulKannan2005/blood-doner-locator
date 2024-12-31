6051e9c312dbd040a915a7ab23291dd752b5d30d
RagulKannan2005 <msdragul154@gmail.com>
2024-12-31 12:21:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,6 @@ createRoot(document.getElementById('root')).render(
     {/* <Registerform /> */}
     {/* <Entrypage /> */}
     {/* <Tcenter /> */}
-    <Seeker />
+    {/* <Seeker /> */}
   </StrictMode>,
 )
