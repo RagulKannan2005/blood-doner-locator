07fa9c965cf1c9082246e0c21df756a05d1e6845
RagulKannan2005 <msdragul154@gmail.com>
2024-12-31 15:39:20 +0530

    Create Nopage.jsx

---
+++ src/pages/Nopage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+
+const Nopage = () => {
+  return (
+    <>
+      <Navbar />
+      <h2>Error 404: Not found</h2>
+    </>
+  );
+};
+
+export default Nopage;
