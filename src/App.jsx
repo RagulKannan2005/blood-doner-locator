fd01a17d5830912daa566f2e0f422d1b322d271d
RagulKannan2005 <msdragul154@gmail.com>
2024-12-31 15:39:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Doner from "./pages/Doner";
+import Seeker from "./pages/Seeker";
+import Nopage from "./pages/Nopage";
+
 import "../src/components/cssfiles/App.css";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
-      <div className="nav-container">
-        <Navbar />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Doner" element={<Doner />} />
+          <Route path="/Seeker" element={<Seeker />} />
+          <Route path="*" element={<Nopage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
