f3e60cf7f8a1e209abec96368bf6c161fdc1938b
RagulKannan2005 <msdragul154@gmail.com>
2024-12-31 14:43:03 +0530

    Update Navbar.jsx
    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../cssfiles/Navbar.css";
+import logo from "../../assets/BD-logo.png";
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="web-name">
+        <img src={logo} alt="Logo" className="logo" />
+        <span>BLink</span>
+      </div>
+      <div className="menu-btn" onClick={toggleMenu}>
+        ☰
+      </div>
+      <ul className={`nav-list ${menuOpen ? "active" : ""}`}>
+        <li className="nav-item">
+          <a href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Seeker</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Donor</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
