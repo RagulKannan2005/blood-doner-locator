9d0b75f47a1f567442f89d674be55f9d372b98d3
RagulKannan2005 <165671801+RagulKannan2005@users.noreply.github.com>
2025-01-02 15:51:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
 import Doner from "./pages/Doner";
 import Seeker from "./pages/Seekerpage";
 import Nopage from "./pages/Nopage";
@@ -19,8 +17,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
           <Route path="/Doner" element={<Doner />} />
           <Route path="/Seeker" element={<Seeker />} />
           <Route path="/Seekerreg" element={<Seekerreg />} />
