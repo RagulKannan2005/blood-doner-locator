5c3190b2f2a90d8725f84f19487ae785ece38b55
RagulKannan2005 <165671801+RagulKannan2005@users.noreply.github.com>
2025-01-02 15:32:44 +0530

    Update Seekerpage.jsx

--- src/pages/Seekerpage.jsx
+++ src/pages/Seekerpage.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
-import '../components/cssfiles/seekerpage.css'
-import { Link } from "react-router-dom";
-
-
+import "../components/cssfiles/Seekerpage.css"
+import { Link, useNavigate } from "react-router-dom";
 const Seekerpage = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Navbar />
-      <div className="need-btn">
-        <button >Need Blood</button>
-      </div>
-      <h2>Seeker page</h2>
+      <h1>Seekerpage</h1>
+     
     </>
   );
 };
