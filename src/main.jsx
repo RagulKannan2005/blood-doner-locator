722be850c70b26a91e94d0108256a40a080de89d
RagulKannan2005 <msdragul154@gmail.com>
2025-01-03 10:38:25 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import './index.css'
 // import Seeker from './components/Seeker'
 // import Registerform from './components/Registerform'
 // import Entrypage from './components/Entrypage'
-// import Tcenter from './components/Tcenter'
+import Tcenter from './components/Tcenter'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <Registerform /> */}
     {/* <Entrypage /> */}
-    {/* <Tcenter /> */}
+    <Tcenter />
     {/* <Seeker /> */}
     
   </StrictMode>,
