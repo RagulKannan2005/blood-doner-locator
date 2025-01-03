d6b1d480c37426dddcd3c1c96272e9dfd447f7a5
RagulKannan2005 <msdragul154@gmail.com>
2025-01-03 14:14:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Home from "./pages/Home";
 import Doner from "./pages/Doner";
 import Seeker from "./pages/Seekerpage";
 import Nopage from "./pages/Nopage";
-import Seekerreg from "./components/SeekerRegisteration"
-import Donerreg from "./pages/Doner"
+import Seekerreg from "./components/SeekerRegisteration";
 import "../src/components/cssfiles/App.css";
 import { useEffect } from "react";
+import Registerdoner from "../src/components/Registerform";
 
 function App() {
   useEffect(() => {}, [Home]);
@@ -20,8 +20,8 @@ function App() {
           <Route path="/Doner" element={<Doner />} />
           <Route path="/Seeker" element={<Seeker />} />
           <Route path="/Seekerreg" element={<Seekerreg />} />
+          <Route path="/Registerdoner" element={<Registerdoner />} />
           <Route path="*" element={<Nopage />} />
-          <Route path="/Donerreg" element={<Donerreg/>}/>
         </Routes>
       </BrowserRouter>
     </>
